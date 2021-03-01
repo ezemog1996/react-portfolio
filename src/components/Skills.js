5aa5cfc4eb4061ac504e1440739697764b11dcca
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-03-01 17:28:23 -0500

    added some icons for mongodb and mysql

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,15 +9,16 @@ function Skills() {
                 <h2 className="mb-5">Skills</h2>
                 <div className="subheading mb-3">Programming Languages & Tools</div>
                 <ul className="list-inline dev-icons">
-                    <li className="list-inline-item"><i className="fab fa-html5"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-react"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-node-js"></i></li>
-                    <li className="list-inline-item"><i className="fab fa-npm"></i></li>
-                    <li className="list-inline-item" style={{'fontSize': '25px'}}>MongoDB</li>
-                    <li className="list-inline-item" style={{'fontSize': '25px'}}>MySQL</li>
-                    <li className="list-inline-item"><i className="fab fa-wordpress"></i></li>
+                    <li className="list-inline-item" title="HTML5"><i className="fab fa-html5"></i></li>
+                    <li className="list-inline-item" title="CSS3"><i className="fab fa-css3-alt"></i></li>
+                    <li className="list-inline-item" title="Bootstrap"><i className="devicon-bootstrap-plain"></i></li>
+                    <li className="list-inline-item" title="JavaScript ES6+"><i className="fab fa-js-square"></i></li>
+                    <li className="list-inline-item" title="Reactjs"><i className="fab fa-react"></i></li>
+                    <li className="list-inline-item" title="Node.js"><i className="fab fa-node-js"></i></li>
+                    <li className="list-inline-item" title="NPM"><i className="fab fa-npm"></i></li>
+                    <li className="list-inline-item" title="MongoDB"><i className="devicon-mongodb-plain"></i></li>
+                    <li className="list-inline-item" title="MySQL"><i className="devicon-mysql-plain"></i></li>
+                    <li className="list-inline-item" title="Wordpress"><i className="fab fa-wordpress"></i></li>
                 </ul>
                 <div className="subheading mb-3">Workflow</div>
                 <ul className="fa-ul mb-0">
